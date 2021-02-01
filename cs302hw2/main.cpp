cd8619249f8d507465871e4d1b095198a7279db6
Garett Pascual-Folster <31081158+GarettPF@users.noreply.github.com>
2021-02-01 10:00:35 -0800

    Init

--- cs302hw2/main.cpp
+++ cs302hw2/main.cpp
@@ -1,6 +1,12 @@
+/*
+* Name: Garett Pascual-Folster, 2001691416, Assignment 2
+* Description: This program takes in a sudoku problem from the user
+*				and solves the puzzle automatically using recursion
+* Input: a .txt file that contains a sudoku puzzle
+* Output: a complete and finished sudoku puzzle based on the input
+*/
 
-
-
+#include "sudoku.h"
 
 int main() {
 
---
+++ cs302hw2/sudoku.h
@@ -0,0 +1,11 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+#include <iostream>
+
+using namespace std;
+
+
+
+
+#endif
