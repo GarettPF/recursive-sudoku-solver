65cbc23fda60cd88f38d97f4e0786417975f2ede
Garett Pascual-Folster <31081158+GarettPF@users.noreply.github.com>
2021-02-03 18:52:19 -0800

    Finished

--- cs302hw2/main.cpp
+++ cs302hw2/main.cpp
@@ -13,15 +13,31 @@ int main() {
 	string filename;
 	int board[9][9];
 
-	do { // Get the .txt file from the user and load the puzzle
-		puzzle.clear();
-		cout << "Enter initial sudoku board file: ";
-		cin >> filename;
-		puzzle.open(filename.c_str());
-	} while (puzzle.fail());
-	load_puzzle(board, puzzle);
+	while (true) {
+		do { // Get the .txt file from the user and load the puzzle
+			puzzle.clear();
+			cout << "Enter initial sudoku board file: ";
+			cin >> filename;
+			if (filename == "NO")
+				return 0;
+			puzzle.open(filename.c_str());
+		} while (puzzle.fail());
+		load_puzzle(board, puzzle);
 
+		// before solving
+		cout << endl << "Initial board read in" << endl << endl;
+		print_puzzle(board);
 
+		// solving...
+		solve_puzzle(board, 0, 0);
+
+		// after solving
+		cout << endl << "Sudoku puzzled solved" << endl << endl;
+		print_puzzle(board);
+		cout << endl;
+
+		puzzle.close();
+	}
 
 	return 0;
 }
--- cs302hw2/sudoku.h
+++ cs302hw2/sudoku.h
@@ -11,6 +11,36 @@ using namespace std;
  * @param board		The 2D array that stores the board state
  * @param input		The input file that contains the user given puzzle
 */
+void load_puzzle(int board[][9], ifstream& input);
+
+/**
+ * @brief			Print the sudoku puzzle in a nice format
+ * @param board		The current sudoku puzzle to print out
+*/
+void print_puzzle(int board[][9]);
+
+/**
+ * @brief			checks the current position of the board to see if the @param num
+ *					will work in that current state of the board
+ * @param board		The board to check
+ * @param row		The row of the position
+ * @param col		The column of the position
+ * @param num		The number to check if valid
+ * @return			True if possible, False otherwise
+*/
+bool check_position(int board[][9], int row, int col, int num);
+
+/**
+ * @brief			The recursive function that will solve the given sudoku puzzle
+ * @param board		The current state of the board to solve
+ * @param row		The current row of the position
+ * @param col		The current col of the position
+ * @return			True if solved; False otherwise
+*/
+bool solve_puzzle(int board[][9], int row, int col);
+
+
+
 void load_puzzle(int board[][9], ifstream& input) {
 	for (int y = 0; y < 9; y++) {
 		for (int x = 0; x < 9; x++) {
@@ -19,10 +49,8 @@ void load_puzzle(int board[][9], ifstream& input) {
 	}
 }
 
-/**
- * @brief			Print the sudoku puzzle in a nice format
- * @param board		The current sudoku puzzle to print out
-*/
+
+
 void print_puzzle(int board[][9]) {
 	cout << "-------------------------------" << endl;
 	for (int y = 0; y < 9; y++) {
@@ -39,6 +67,63 @@ void print_puzzle(int board[][9]) {
 		cout << "|" << endl;
 	}
 	cout << "-------------------------------" << endl;
+}
+
+
+
+bool check_position(int board[][9], int row, int col, int num) {
+	for (int x = 0; x < 9; x++) { // check the current row
+		if (board[row][x] == num)
+			return false;
+	}
+	for (int y = 0; y < 9; y++) { // check the current col
+		if (board[y][col] == num)
+			return false;
+	}
+
+	// ex. Section 1 is X=0, Y=0; Section 9 is X=2, Y=2;
+	int sectionX = (int)col/3, 
+		sectionY = (int)row/3; 
+	
+	for (int x = 0; x < 3; x++) { // check the current section
+		for (int y = 0; y < 3; y++) {
+			if (board[(sectionY * 3) + y][(sectionX * 3) + x] == num)
+				return false;
+		}
+	}
+
+	// if passed all 3 tests, the number currently works
+	return true;
+}
+
+
+
+bool solve_puzzle(int board[][9], int row, int col) {
+	int _row, _col;
+
+
+	// check if puzzle is solved
+	if (row == 9)
+		return true;
+
+	// calculate the next position
+	_row = (col == 8) ? row + 1 : row;
+	_col = (col == 8) ? 0 : col + 1;
+
+	// check if on non blank position
+ 	if (board[row][col] != 0) {
+		return (solve_puzzle(board, _row, _col));
+	} else { // position is blank
+		for (int num = 1; num <= 9; num++) {
+			if (check_position(board, row, col, num)) {
+				board[row][col] = num;
+				if (solve_puzzle(board, _row, _col)) // puzzle is solved
+					return true;
+			}
+		}
+		board[row][col] = 0;
+		return false;
+	}
 }
 
 
